56042ad671032ce766eee62aaa13af914d5b1cec
EddWills95 <edd.williams@me.com>
2022-01-21 16:07:05 +0000

    Initial Svelte edd-williams

---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<main class="h-100 w-100 p-4 bg-gunmetal">
+	<header class="flex justify-between items-center">
+        <a href="/" class="h-8 w-12 border-white border-2 flex justify-center items-center hover:bg-slate-500">Edd</a>
+        <nav>
+            <ul class="list-none flex gap-2">
+                <li class="h-8 w-auto px-2 flex justify-center items-center border-2 border-transparent hover:border-white"><a href="#about">About</a></li>
+                <li class="h-8 w-auto px-2 flex justify-center items-center border-2 border-transparent hover:border-white"><a href="#experience">Experience</a></li>
+                <li class="h-8 w-auto px-2 flex justify-center items-center border-2 border-transparent hover:border-white"><a href="#work">Work</a></li>
+                <li class="h-8 w-auto px-2 flex justify-center items-center border-2 border-transparent hover:border-white"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="flex flex-col gap-4 justify-center">
+        <section id="banner" class="w-100 h-96 p-24 flex items-start text-2xl flex-col justify-center">
+            <h1 class="flex flex-col gap-2">
+                <span class="text-base font-mono font-thin">Hi 👋<br></span>
+                <span class="text-4xl text-burnt-sienna">I'm Edd <br></span>
+                <span class="text-3xl flex flex-col gap-2 text-light-cyan">
+                    I build things online 
+                    <span class=" inline-block ml-16">on raspberry pis</span> 
+                    <span class=" inline-block ml-24">and sometimes in reality</span>
+                </span>
+            </h1>
+        </section>
+
+        <section id="about" class="w-10/12">
+            <h2 class="underline">About Edd</h2>
+            <p>I'm Edd 👋</p>
+        </section>
+        
+        <section id="experience"></section>
+        
+        <section id="work"></section>
+        
+        <section id="contact"></section>
+    </div>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		colors: {
+			'bdazzled-blue': '#3d5a80ff',
+			'pale-cerulean': '#98c1d9ff',
+			'light-cyan': '#e0fbfcff',
+			'burnt-sienna': '#ee6c4dff',
+			gunmetal: '#293241ff'
+		},
+		extend: {}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
