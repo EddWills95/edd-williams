86a86b26c39149553bbad52292465b053bb3b1e0
EddWills95 <edd.williams@me.com>
2022-01-23 00:10:55 +0000

    refactor to about component

---
+++ src/components/about.svelte
@@ -0,0 +1,64 @@
+<div class="flex flex-col flex-wrap justify-center items-center gap-4 sm:flex-row">
+	<article class="flex flex-col gap-4 leading-relaxed">
+		<p class="relative">
+			Hey 👋 I'm Edd and I've been messing with technology for as long as I can remember and since
+			2017, have been professionally. I started with <span class="italic text-burnt-sienna-400"
+				>javascript animations</span
+			>
+			and <span class="italic text-burnt-sienna-400">python web scrapers</span>, fascinated how the
+			virtual and real world could come together.
+			<span class="hidden sm:block text-2xl absolute -right-6 top-0">🌐</span>
+		</p>
+
+		<p class="relative">
+			Bitcoin came into my life around 2012 when my father came home babbling about <span
+				class="italic text-burnt-sienna-400">some internet currency</span
+			>
+			that "the guys in the office were going crazy for". From there, I was hooked.
+			<span class="hidden sm:block text-2xl absolute -left-12 top-0">
+				<svg class="inline ml-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+					><g fill="none" fill-rule="evenodd"
+						><circle cx="16" cy="16" r="16" fill="#F7931A" /><path
+							fill="#FFF"
+							fill-rule="nonzero"
+							d="M23.189 14.02c.314-2.096-1.283-3.223-3.465-3.975l.708-2.84-1.728-.43-.69 2.765c-.454-.114-.92-.22-1.385-.326l.695-2.783L15.596 6l-.708 2.839c-.376-.086-.746-.17-1.104-.26l.002-.009-2.384-.595-.46 1.846s1.283.294 1.256.312c.7.175.826.638.805 1.006l-.806 3.235c.048.012.11.03.18.057l-.183-.045-1.13 4.532c-.086.212-.303.531-.793.41.018.025-1.256-.313-1.256-.313l-.858 1.978 2.25.561c.418.105.828.215 1.231.318l-.715 2.872 1.727.43.708-2.84c.472.127.93.245 1.378.357l-.706 2.828 1.728.43.715-2.866c2.948.558 5.164.333 6.097-2.333.752-2.146-.037-3.385-1.588-4.192 1.13-.26 1.98-1.003 2.207-2.538zm-3.95 5.538c-.533 2.147-4.148.986-5.32.695l.95-3.805c1.172.293 4.929.872 4.37 3.11zm.535-5.569c-.487 1.953-3.495.96-4.47.717l.86-3.45c.975.243 4.118.696 3.61 2.733z"
+						/></g
+					></svg
+				>
+			</span>
+		</p>
+
+		<p class="relative">
+			I started exploring battery tech between careers, becoming totally fanatical with
+			<span class="italic text-burnt-sienna-400">repurposing</span> and
+			<span class="italic text-burnt-sienna-400">upcycling</span> them. I don't think the importance
+			of energy storage can be overstated for the future
+			<span class="hidden sm:block text-2xl absolute -right-6 top-0"> 🔋 </span>
+		</p>
+
+		<p class="relative">
+			With an education in politics, I come at software from a slightly different angle. I love
+			building products <span class="italic text-burnt-sienna-400">for people</span> and
+			<span class="italic text-burnt-sienna-400">with people</span>. I thrive in colaberative teams;
+			researching, designing and building end to end features.
+			<span class="hidden sm:block text-2xl absolute -left-10 top-0">🗳</span>
+		</p>
+
+		<div class="flex w-full gap-8 justify-center">
+			<div>
+				<p class="italic">A flavour of my toolset:</p>
+				<ul class="list-disc list-inside">
+					<li>React</li>
+					<li>Typescript</li>
+					<li>Node</li>
+					<li>AWS</li>
+					<li>Docker</li>
+					<li>Tailwind</li>
+				</ul>
+			</div>
+			<div class="w-auto h-48 offset-border">
+				<img class="z-10 w-48 h-48 rounded" src="./edd.jpeg" alt="Edd" />
+			</div>
+		</div>
+	</article>
+</div>
